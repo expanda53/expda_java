73670ce98cfcdc9832307e476f343e789b2a2ab7
Kende-László <laszlo.kende@gmail.com>
2017-01-27 11:22:57 +0100

    toptyre leltár hkód és eankód hossz vizsgálatok

--- application/expda/lua/leltar/bcode1.lua
+++ application/expda/lua/leltar/bcode1.lua
@@ -2,8 +2,13 @@ require 'model/Luafunc'
 local params = {...}
 ui=params[1]
 hkodc = params[2]:gsub(':','')
+
 if (hkodc:len()>10) then
   ui:showMessage('helykód hossza nem lehet több, mint 10 karakter!')
+  ui:executeCommand('valueto','lhkoc','')
+  ui:executeCommand('setfocus','lhkoc','')
+elseif (hkodc==" " or hkodc=="") then
+  ui:showMessage('helykód nem lehet üres!')
   ui:executeCommand('valueto','lhkoc','')
   ui:executeCommand('setfocus','lhkoc','')
 else 
